f38ded28c98f531dd5e65c50602003e23f3d56ee
jishaofeng89 <1195957057@qq.com>
2019-11-05 18:25:16 +0800

    1.2

--- lib/pages/lesson1.dart
+++ lib/pages/lesson1.dart
@@ -8,14 +8,55 @@ class Lession1Page extends StatefulWidget {
 }
 
 class _Lession1PageState extends State<Lession1Page> {
+
+  // 最里面的通话的视图
+  Widget _viewRows() {
+    return Container(
+      color: Colors.green,
+    );
+  }
+
+  // 右上角通话的小窗口
+  Widget _smallWindow() {
+    // GestureDetector跟随child大小
+    return GestureDetector(
+      onDoubleTap: () {},
+      onTap: () {},
+      child: Align(
+        alignment: Alignment.topRight,
+        child: Container(
+          color: Colors.red,
+          width: 110,
+          height: 150,
+          margin: EdgeInsets.only(top: 40, right: 40),
+        ),
+      ),
+    );
+  }
+
+  // 消息列表
+  Widget _panel() {
+    return Container();
+  }
+
+  // 挂断，静音等工具栏
+  Widget _toolbar() {
+    return Container();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('1.布局'),
-      ),
+      // appBar: AppBar(
+      //   title: Text('1.布局'),
+      // ),
       body: Center(
-        child: Text('第一课'),
+        child: Stack(
+          children: <Widget>[
+            _viewRows(),
+            _smallWindow(),
+          ],
+        ),
       ),
     );
   }
